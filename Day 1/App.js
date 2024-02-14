59c1ab8a9e033ea35d9a949e188bf6a8c20700c6
Md Yusuf Ansari <100552609+iamyusuf01@users.noreply.github.com>
2024-02-14 12:33:42 +0530

    Add files via upload

---
+++ Day 1/App.js
@@ -0,0 +1,31 @@
+// const heading = document.createElement("h1",{
+//     id: "title",
+// },
+// "Namaste everyone !")
+
+// const heading1 = React.createElement(
+//     "h1",
+//     {
+//         id: "title",
+//     },
+//     "Heading 1"
+// );
+
+//  const heading2 = React.createElement(
+//     "h2",
+//     {
+//         id: "title",
+//     },
+//     "Heading 2"
+// );
+
+// const container = React.createElement("div",{ id: "container" },[
+//     heading1,
+//     heading2
+// ]);
+
+// const root = React.createRoot(document.getElementById("root"));
+// root.render(container);
+
+// Passing a react element inside the root
+// root.render(heading)
