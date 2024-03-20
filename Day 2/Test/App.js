6e5e87abbc9cc826ac6d74fcb711f5fe688fd058
Md Yusuf Ansari <100552609+iamyusuf01@users.noreply.github.com>
2024-03-20 19:47:36 +0530

    Add files via upload

---
+++ Day 2/Test/App.js
@@ -0,0 +1,94 @@
+// function x() {
+//     const a = 10;
+// }
+// var xyz = 30;
+// x();// function exucution context is created
+// var x = function () {
+//     console.log("I  am anonymouse function ");
+
+// }
+//     In ES6
+// const fn = ()=>{}
+
+// ----------------------------------------------------
+
+// console.log(this);
+// const person = {
+//   name: "Yusuf",
+//   print: function () {
+//     console.log(this);
+//   },
+// };
+// const person2 = {
+//   name: "Aysha",
+// };
+// person.print();
+// person.print.call();
+// person.print.call(this);
+// person.print.call(person2);
+// person.print.call(person);
+// -----------------------------------------------
+
+// const obj = {
+//   firstName: "Yusuf",
+//   printName: () => {
+//     console.log(this);
+//   },
+//   printName2: function () {
+//     console.log(this);
+//   },
+// };
+// const obj2 = {
+//   firstName: "Aysha",
+// };
+
+// obj.printName();
+// obj.printName2();
+// obj.printName.call();
+// obj.printName2.call();
+// ----------------------------------------------
+/*
+
+
+
+INTERVIEWS      
+
+-Luck
+-Company don't train their interviwers well 
+- A person can be a good Software Engineer but a Bad Interviwers
+-LUCK - Can't control 
+
+- Our Preparation
+  -(50%)technical  skill  5/5 days
+    - Everyone knows what to study 
+  -(50%)communication skills 0/5 days
+        - speak your thoughts while you code 
+       		 - practice to speak even when you're coding alone
+  		-If you can't explain , then (Interviewer thinks ) you don't know 
+
+		-you don't work alone in the company
+		-companies expect you to have good communication skills
+	    
+		- It's a brutal truth , you should know english 
+
+	- Mock Interviews 
+- preparation on the interview day 
+	- you should NOT be hustling at last moment 
+	- pen/paper
+	- water bottle 
+	- charger
+	- keep your camera open
+	- keep your phone on silent 
+	-router mini ups 
+
+
+
+CONFIDENCE COMES FROM PREPARATION 
+	(not just your technical prep)
+
+		
+
+
+
+
+*/
